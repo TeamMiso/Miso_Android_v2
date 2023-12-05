b771588aa191c55d6ca96b545799d0a94af48fa7
송현서 <s22026@gsm.hs.kr>
2023-12-05 19:40:31 +0900

    :tada: :: Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Miso_Android_v2"
+include(":app")
+ 
