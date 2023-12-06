d38a11ea711cc5ea4cbaf27d61b178d2b775d770
GSM-Minseok <ms0818000@gmail.com>
2023-12-06 20:24:23 +0900

    :gear: :: Set Project Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
-    id("com.android.library") version "8.1.4" apply false
+    id(ProjectProperties.Gradle.APPLICATION) version "8.1.4" apply false
+    id(ProjectProperties.Gradle.KOTLIN) version "1.8.10" apply false
+    id(ProjectProperties.Gradle.LIBRARY) version "8.1.4" apply false
 }
