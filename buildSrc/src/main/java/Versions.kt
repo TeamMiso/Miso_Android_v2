7aaf0cf7735b35df75dd36103dd18bd3a4ff4eea
GSM-Minseok <ms0818000@gmail.com>
2023-12-06 19:53:42 +0900

    :memo: :: Add Versions

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,70 @@
+import org.gradle.api.JavaVersion
+
+object Versions {
+    //gradle
+    const val GRADLE_ANDROID = "7.4.2"
+    const val GRADLE_KOTLIN = "1.8.10"
+
+    //coroutines
+    const val KOTLINX_COROUTINES = "1.6.0"
+    const val COROUTINE = "1.3.9"
+
+    //github
+    const val GITHUB_API = "8.1.0"
+    const val GITHUB_CORE = "8.1.0"
+
+    //compose
+    const val COMPOSE = "1.4.2"
+    const val ACTIVITY_COMPOSE = "1.7.2"
+    const val COMPOSE_MATERIAL = "1.4.3"
+    const val COMPOSE_MATERIAL3 = "1.1.1"
+
+    //appCompat
+    const val CORE_KTX = "1.5.0"
+
+    const val APP_COMPAT = "1.6.1"
+    const val ACCOMPANIST = "0.31.2-alpha"
+
+    const val PREFERENCE_KTX = "1.2.0"
+    const val PREFERENCES = "1.0.0"
+
+    const val LIFECYCLE_KTX = "2.3.1"
+
+    const val ROOM = "2.3.0"
+
+    const val CAMERA = "1.3.0-beta01"
+
+    //hilt
+    const val HILT = "2.45"
+
+    //material
+    const val MATERIAL = "1.9.0"
+
+    //googlePlay
+    const val GMS_PLAY_SERVICE_AUTH = "19.0.0"
+    const val GMS_GOOGLE_SERVICE = "4.3.15"
+
+    //firebase
+    const val FIREBASE_DATABASE = "20.3.0"
+    const val FIREBASE_BOM = "32.5.0"
+
+    //remote
+    const val RETROFIT = "2.7.1"
+    const val OKHTTP = "4.10.0"
+    const val GSON = "2.9.0"
+    const val MOSHI = "1.13.0"
+
+    //junit
+    const val JUNIT = "4.13.2"
+    const val ANDROID_JUNIT = "1.1.2"
+    const val ESPRESSO_CORE = "3.5.1"
+
+    //coil
+    const val COIL_VERSION = "2.4.0"
+
+    //navigation
+    const val NAVIGATION = "2.6.0"
+
+    //markdown
+    const val MARKDOWN = "0.3.6"
+}
