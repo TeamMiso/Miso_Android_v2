8269fc3005dc416e346e3e1778d5d7bc12617dcf
GSM-Minseok <ms0818000@gmail.com>
2023-12-06 20:23:57 +0900

    :memo: :: Add ProjectProperties

---
+++ buildSrc/src/main/java/ProjectProperties.kt
@@ -0,0 +1,38 @@
+import org.gradle.api.JavaVersion
+
+object ProjectProperties {
+
+    object Versions{
+        const val BUILD_GRADLE = "4.2.1"
+        const val COMPILE_SDK_VERSION = 33
+        const val BUILD_TOOLS_VERSION = "32.0.0"
+        const val MIN_SDK_VERSION = 26
+        const val TARGET_SDK_VERSION = 33
+
+        val JAVA_VERSION = JavaVersion.VERSION_1_8
+    }
+
+    object Gradle {
+        const val APPLICATION = "com.android.application"
+        const val LIBRARY = "com.android.library"
+        const val KOTLIN = "org.jetbrains.kotlin.android"
+        const val KAPT = "kotlin-kapt"
+
+        const val DI = "com.miso.miso_android_v2"
+        const val DATA = "com.miso.data"
+        const val DESIGN_SYSTEM = "com.miso.design_system"
+        const val DOMAIN = "com.miso.domain"
+        const val PERSENTATION = "com.miso.presentation"
+    }
+
+    object Files {
+        const val CONSUMER_PROGUARDFILES = "consumer-rules.pro"
+        const val DEFAULT_PROGUARDFILES = "proguard-android-optimize.txt"
+        const val PROGUARDFILES = "proguard-rules.pro"
+    }
+
+    object TestProperties {
+        const val TEST_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+}
