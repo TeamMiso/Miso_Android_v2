c688ee1b9128ad5e2a0fa66d0e23b17a83d6c462
GSM-Minseok <ms0818000@gmail.com>
2023-12-06 20:08:07 +0900

    :gear: :: Set Kotiln Dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.android.library") version "8.1.4" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,5 +14,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "Miso_Android_v2"
-include(":app")
- 
+include(":di")
