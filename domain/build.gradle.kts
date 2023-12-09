280cd3c194b457295339a7be7ddb865d4cfc9c3d
GSM-Minseok <ms0818000@gmail.com>
2023-12-09 13:46:45 +0900

    :memo: :: Set domain gradle di

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id(ProjectProperties.Gradle.KOTLIN)
     id(ProjectProperties.Gradle.LIBRARY)
+    id(ProjectProperties.Gradle.HILT)
+    id(ProjectProperties.Gradle.KAPT)
 }
 
 android {
@@ -33,6 +35,10 @@ android {
 }
 
 dependencies {
+    //hilt
+    implementation(Dependency.Google.HILT_ANDROID)
+    kapt(Dependency.Google.HILT_ANDROID_COMPILER)
+
     //unitTest
     testImplementation(Dependency.Test.JUNIT)
 
