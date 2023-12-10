2bc796a0e3f57fb6369a1de75330fa3b85803a89
송현서 <s22026@gsm.hs.kr>
2023-12-11 08:30:25 +0900

    :lipstick: :: Add color

---
+++ design-system/src/main/java/com/miso/design_system/theme/ColorTheme.kt
@@ -0,0 +1,21 @@
+package com.miso.design_system.theme
+
+import androidx.compose.ui.graphics.Color
+
+abstract class ColorTheme {
+
+    abstract val PRIMARY: Color
+
+    abstract val BULE1: Color
+
+    abstract val RED1: Color
+
+    abstract val GREYSCALE1: Color
+    abstract val GREYSCALE2: Color
+    abstract val GREYSCALE3: Color
+    abstract val GREYSCALE4: Color
+    abstract val GREYSCALE5: Color
+
+    abstract val BLACK: Color
+    abstract val WHITE: Color
+}
---
+++ design-system/src/main/java/com/miso/design_system/theme/color/LightColor.kt
@@ -0,0 +1,21 @@
+package com.miso.design_system.theme.color
+
+import androidx.compose.ui.graphics.Color
+import com.miso.design_system.theme.ColorTheme
+
+object LightColor : ColorTheme() {
+    override val PRIMARY = Color(0xFF25D07D)
+
+    override val BULE1 = Color(0xFF3484DB)
+
+    override val RED1 = Color(0xFFDB3734)
+
+    override val GREYSCALE1 = Color(0x26000000)
+    override val GREYSCALE2 = Color(0xFF595959)
+    override val GREYSCALE3 = Color(0xFFBFBFBF)
+    override val GREYSCALE4 = Color(0xFFD9D9D9)
+    override val GREYSCALE5 = Color(0xFFF2F2F2)
+
+    override val BLACK = Color(0xFF000000)
+    override val WHITE = Color(0xFFFFFFFF)
+}
