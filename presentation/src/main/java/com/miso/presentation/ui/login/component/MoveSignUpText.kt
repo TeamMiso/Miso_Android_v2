b52304a1c48b239c26f359c386b2d7dd37102e73
송현서 <s22026@gsm.hs.kr>
2023-12-11 15:59:46 +0900

    :lipstick: :: Add component

---
+++ presentation/src/main/java/com/miso/presentation/ui/login/component/MoveSignUpText.kt
@@ -0,0 +1,29 @@
+package com.miso.presentation.ui.login.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import com.miso.design_system.theme.MisoTheme
+
+@Composable
+fun MoveSignUpText(onClick: () -> Unit) {
+    MisoTheme { colors, typography ->
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "회원가입",
+                modifier = Modifier.clickable { onClick() },
+                style = typography.textSmall,
+                fontWeight = FontWeight.Normal,
+                color = colors.BULE1
+            )
+        }
+    }
+}
