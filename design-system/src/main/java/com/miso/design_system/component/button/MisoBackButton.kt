1aebbeb36fdca09a547da4a24f07a8df2279cd47
송현서 <s22026@gsm.hs.kr>
2023-12-12 14:04:31 +0900

    :lipstick: :: Add MisoBackButton

---
+++ design-system/src/main/java/com/miso/design_system/component/button/MisoBackButton.kt
@@ -0,0 +1,33 @@
+package com.miso.design_system.component.button
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import com.miso.design_system.theme.MisoTheme
+
+@Composable
+fun MisoBackButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    MisoTheme { colors, typography ->
+        Row(
+            modifier = modifier.clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) { onClick() },
+        ) {
+            Text(
+                text = "< 돌아가기",
+                style = typography.textMedium,
+                fontWeight = FontWeight.SemiBold,
+                color = colors.BULE1
+            )
+        }
+    }
+}
