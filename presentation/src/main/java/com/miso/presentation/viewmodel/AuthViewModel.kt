faa463d452c5cc243de5c797b04c6d9f14b197c5
송현서 <s22026@gsm.hs.kr>
2023-12-13 18:17:45 +0900

    :sparkles: :: Add login to AuthViewModel

--- presentation/src/main/java/com/miso/presentation/viewmodel/AuthViewModel.kt
+++ presentation/src/main/java/com/miso/presentation/viewmodel/AuthViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.miso.viewmodel.util.Event
 import com.example.miso.viewmodel.util.errorHandling
+import com.miso.domain.model.auth.request.AuthLogInRequestModel
 import com.miso.domain.model.auth.request.AuthSignUpRequestModel
+import com.miso.domain.model.auth.response.AuthLogInResponseModel
+import com.miso.domain.usecase.auth.AuthLogInUseCase
 import com.miso.domain.usecase.auth.AuthSignUpUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,10 +19,14 @@ import javax.inject.Inject
 @HiltViewModel
 class AuthViewModel @Inject constructor(
     private val authSignUpUseCase: AuthSignUpUseCase,
+    private val authLogInUseCase: AuthLogInUseCase,
 ) : ViewModel() {
 
     private val _authSignUpResponse = MutableStateFlow<Event<Unit>>(Event.Loading)
     val authSignUpResponse = _authSignUpResponse.asStateFlow()
+
+    private val _authLogInResponse = MutableStateFlow<Event<AuthLogInResponseModel>>(Event.Loading)
+    val authLogInResponse = _authLogInResponse.asStateFlow()
 
     fun authSignUp(body: AuthSignUpRequestModel) = viewModelScope.launch {
         authSignUpUseCase(
@@ -32,6 +39,20 @@ class AuthViewModel @Inject constructor(
             }
         }.onFailure {
             _authSignUpResponse.value = it.errorHandling()
+        }
+    }
+
+    fun authLogIn(body: AuthLogInRequestModel) = viewModelScope.launch {
+        authLogInUseCase(
+            body = body
+        ).onSuccess {
+            it.catch { remoteError ->
+                _authLogInResponse.value = remoteError.errorHandling()
+            }.collect { response ->
+                _authLogInResponse.value = Event.Success(data = response)
+            }
+        }.onFailure {
+            _authLogInResponse.value = it.errorHandling()
         }
     }
 }
