632cde85e6c0dd0a2730640ef317de61eaf338c5
송현서 <s22026@gsm.hs.kr>
2023-12-13 12:15:56 +0900

    :sparkles: :: Add sign up to AuthDatasource

---
+++ data/src/main/java/com/miso/data/remote/datasource/auth/AuthDataSourceImpl.kt
@@ -0,0 +1,22 @@
+package com.miso.data.remote.datasource.auth
+
+import com.miso.data.remote.api.AuthAPI
+import com.miso.data.remote.dto.auth.request.AuthSignUpRequest
+import com.miso.data.util.MisoApiHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class AuthDataSourceImpl @Inject constructor(
+    private val api: AuthAPI
+): AuthDataSource {
+    override suspend fun authSignUp(body: AuthSignUpRequest): Flow<Unit> = flow {
+        emit(
+            MisoApiHandler<Unit>()
+                .httpRequest { api.authSignUp(body = body) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+}
