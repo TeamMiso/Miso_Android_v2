4f2e914664cd7d4a77783e2c917f2de54839cc1f
GSM-Minseok <ms0818000@gmail.com>
2023-12-15 11:35:07 +0900

    :sparkles: :: Add CameraActivity.kt

---
+++ presentation/src/main/java/com/miso/presentation/ui/camera/CameraActivity.kt
@@ -0,0 +1,10 @@
+package com.miso.presentation.ui.camera
+
+import androidx.activity.compose.setContent
+import com.miso.presentation.ui.base.BaseActivity
+
+class CameraActivity : BaseActivity() {
+    override fun init() {
+        setContent {}
+    }
+}
