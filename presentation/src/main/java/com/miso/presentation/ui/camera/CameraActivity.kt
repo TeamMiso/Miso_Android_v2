8c500631653b349ef2ce8336ceb1d462b9d35741
GSM-Minseok <ms0818000@gmail.com>
2023-12-15 11:35:34 +0900

    :sparkles: :: Add nav to camera screen

--- presentation/src/main/java/com/miso/presentation/ui/camera/CameraActivity.kt
+++ presentation/src/main/java/com/miso/presentation/ui/camera/CameraActivity.kt
@@ -1,10 +1,37 @@
 package com.miso.presentation.ui.camera
 
+import android.util.Log
 import androidx.activity.compose.setContent
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.miso.presentation.ui.base.BaseActivity
+import com.miso.presentation.ui.camera.screen.CameraScreen
+
+enum class CameraPage(val value: String) {
+    Camera("Camera")
+}
 
 class CameraActivity : BaseActivity() {
+    private lateinit var navController: NavController
     override fun init() {
-        setContent {}
+        setContent {
+            navController = rememberNavController()
+            navController.addOnDestinationChangedListener { controller, destination, arguments ->
+
+            }
+            NavHost(
+                navController = navController as NavHostController,
+                startDestination = CameraPage.Camera.value
+            ) {
+                composable(CameraPage.Camera.name) {
+                    CameraScreen(
+                        context = this@CameraActivity
+                    )
+                }
+            }
+        }
     }
 }
