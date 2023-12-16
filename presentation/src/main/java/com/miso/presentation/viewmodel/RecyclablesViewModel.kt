a75d6aef4936522c53104060edabc5837c543541
송현서 <s22026@gsm.hs.kr>
2023-12-16 16:21:37 +0900

    :sparkles: :: Add searchable list to RecyclablesViewModel

--- presentation/src/main/java/com/miso/presentation/viewmodel/RecyclablesViewModel.kt
+++ presentation/src/main/java/com/miso/presentation/viewmodel/RecyclablesViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.viewModelScope
 import com.example.miso.viewmodel.util.Event
 import com.example.miso.viewmodel.util.errorHandling
 import com.miso.domain.model.recyclables.response.SearchResponseModel
+import com.miso.domain.model.recyclables.response.SearchableListResponseModel
 import com.miso.domain.usecase.recyclables.SearchUseCase
+import com.miso.domain.usecase.recyclables.SearchableListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -15,10 +17,14 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RecyclablesViewModel @Inject constructor(
-    private val searchUseCase: SearchUseCase
+    private val searchUseCase: SearchUseCase,
+    private val searchableListUseCase: SearchableListUseCase
 ) : ViewModel() {
     private val _searchResponse = MutableStateFlow<Event<SearchResponseModel>>(Event.Loading)
     val searchResponse = _searchResponse.asStateFlow()
+
+    private val _searchableListResponse = MutableStateFlow<Event<SearchableListResponseModel>>(Event.Loading)
+    val searchableListResponse = _searchableListResponse.asStateFlow()
 
     fun search(search: String) = viewModelScope.launch {
         searchUseCase(search = search)
@@ -30,6 +36,19 @@ class RecyclablesViewModel @Inject constructor(
                 }
             }.onFailure {
                 _searchResponse.value = it.errorHandling()
+            }
+    }
+
+    fun searchableList() = viewModelScope.launch {
+        searchableListUseCase()
+            .onSuccess {
+                it.catch { remoteError ->
+                    _searchableListResponse.value = remoteError.errorHandling()
+                }.collect { response ->
+                    _searchableListResponse.value = Event.Success(data = response)
+                }
+            }.onFailure {
+                _searchableListResponse.value = it.errorHandling()
             }
     }
 }
