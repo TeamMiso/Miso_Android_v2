d4268de687e1e2ddcd6787e27eaffc2221ce2096
GSM-Minseok <ms0818000@gmail.com>
2023-12-17 16:32:20 +0900

    :lipstick: Modify CameraResultPreview.kt

---
+++ presentation/src/main/java/com/miso/presentation/ui/camera/component/CameraResultPreview.kt
@@ -0,0 +1,38 @@
+package com.miso.presentation.ui.camera.component
+
+import android.graphics.Bitmap
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.miso.design_system.R
+import com.miso.design_system.theme.MisoTheme
+
+@Composable
+fun CameraResultPreview() {
+    MisoTheme { colors, typography ->
+        Image(
+            painter = painterResource(id = R.drawable.ic_miso),
+            modifier = Modifier.size(328.dp, 400.dp),
+            contentScale = ContentScale.Crop,
+            contentDescription = "camera result preview"
+        )
+        Spacer(modifier = Modifier.height(40.dp))
+        Text(
+            text = "사진을 잘라 더 정확히 검색해 보아요 :)",
+            style = typography.textSmall,
+            fontWeight = FontWeight(400),
+            color = colors.GREYSCALE3
+        )
+    }
+}
