c587cab727c19533406f76f56fa5719d0de33b4b
GSM-Minseok <ms0818000@gmail.com>
2023-12-17 12:09:06 +0900

    :lipstick: :: Add permission control

--- presentation/src/main/java/com/miso/presentation/ui/camera/CameraActivity.kt
+++ presentation/src/main/java/com/miso/presentation/ui/camera/CameraActivity.kt
@@ -1,14 +1,23 @@
 package com.miso.presentation.ui.camera
 
+import android.Manifest
 import android.util.Log
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.accompanist.permissions.shouldShowRationale
 import com.miso.presentation.ui.base.BaseActivity
 import com.miso.presentation.ui.camera.screen.CameraScreen
+import com.miso.presentation.ui.util.PermissionHandlerActions
 
 enum class CameraPage(val value: String) {
     Camera("Camera")
@@ -16,20 +25,43 @@ enum class CameraPage(val value: String) {
 
 class CameraActivity : BaseActivity() {
     private lateinit var navController: NavController
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun init() {
         setContent {
             navController = rememberNavController()
             navController.addOnDestinationChangedListener { controller, destination, arguments ->
 
             }
+            val showPermissionDialog = remember { mutableStateOf(false) }
+
+            val permissionsList = listOfNotNull(
+                Manifest.permission.CAMERA
+            )
+            val permissionState = rememberMultiplePermissionsState(permissions = permissionsList)
+
+            LaunchedEffect(key1 = Unit) {
+                if (!permissionState.permissions[0].status.isGranted && !permissionState.permissions[0].status.shouldShowRationale) run {
+                    permissionState.permissions[0].launchPermissionRequest()
+                }
+            }
             NavHost(
                 navController = navController as NavHostController,
                 startDestination = CameraPage.Camera.value
             ) {
                 composable(CameraPage.Camera.name) {
-                    CameraScreen(
-                        context = this@CameraActivity
-                    )
+                    if (!permissionState.permissions[0].status.isGranted) {
+                        showPermissionDialog.value = true
+                        PermissionHandlerActions(
+                            permissionState = permissionState,
+                            showPermissionDialog = showPermissionDialog,
+                            context = this@CameraActivity
+                        )
+                    }else {
+                        showPermissionDialog.value = false
+                        CameraScreen(
+                            context = this@CameraActivity
+                        )
+                    }
                 }
             }
         }
