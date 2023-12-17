8118c451acf3a0c342febf170dc19c9c07819326
GSM-Minseok <ms0818000@gmail.com>
2023-12-18 01:37:17 +0900

    :memo: :: Add getAiAnswerList

--- data/src/main/java/com/miso/data/remote/api/RecyclablesAPI.kt
+++ data/src/main/java/com/miso/data/remote/api/RecyclablesAPI.kt
@@ -1,9 +1,13 @@
 package com.miso.data.remote.api
 
+import com.miso.data.remote.dto.recyclables.request.AiRequest
+import com.miso.data.remote.dto.recyclables.response.AiListResponse
 import com.miso.data.remote.dto.recyclables.response.ResultResponse
 import com.miso.data.remote.dto.recyclables.response.SearchResponse
 import com.miso.data.remote.dto.recyclables.response.SearchableListResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface RecyclablesAPI {
@@ -19,4 +23,9 @@ interface RecyclablesAPI {
     suspend fun result(
         @Query("recyclablesType") recyclablesType: String
     ): ResultResponse
+
+    @POST("recyclables/process")
+    suspend fun getAiAnswerList(
+        @Body body: AiRequest
+    ): AiListResponse
 }
