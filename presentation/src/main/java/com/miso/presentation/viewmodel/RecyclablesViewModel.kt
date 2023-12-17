fcbef378f1b202e07e79894436e4ad676805409b
송현서 <s22026@gsm.hs.kr>
2023-12-17 13:57:27 +0900

    :sparkles: :: Add result to RecyclablesViewModel

--- presentation/src/main/java/com/miso/presentation/viewmodel/RecyclablesViewModel.kt
+++ presentation/src/main/java/com/miso/presentation/viewmodel/RecyclablesViewModel.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.miso.viewmodel.util.Event
 import com.example.miso.viewmodel.util.errorHandling
+import com.miso.domain.model.recyclables.response.ResultResponseModel
 import com.miso.domain.model.recyclables.response.SearchResponseModel
 import com.miso.domain.model.recyclables.response.SearchableListModel
 import com.miso.domain.model.recyclables.response.SearchableListResponseModel
+import com.miso.domain.usecase.recyclables.ResultUseCase
 import com.miso.domain.usecase.recyclables.SearchUseCase
 import com.miso.domain.usecase.recyclables.SearchableListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -21,13 +23,17 @@ import javax.inject.Inject
 @HiltViewModel
 class RecyclablesViewModel @Inject constructor(
     private val searchUseCase: SearchUseCase,
-    private val searchableListUseCase: SearchableListUseCase
+    private val searchableListUseCase: SearchableListUseCase,
+    private val resultUseCase: ResultUseCase,
 ) : ViewModel() {
     private val _searchResponse = MutableStateFlow<Event<SearchResponseModel>>(Event.Loading)
     val searchResponse = _searchResponse.asStateFlow()
 
     private val _searchableListResponse = MutableStateFlow<Event<SearchableListResponseModel>>(Event.Loading)
     val searchableListResponse = _searchableListResponse.asStateFlow()
+
+    private val _resultResponse = MutableStateFlow<Event<ResultResponseModel>>(Event.Loading)
+    val resultResponse = _resultResponse.asStateFlow()
 
     var title = mutableStateOf("")
         private set
@@ -39,6 +45,20 @@ class RecyclablesViewModel @Inject constructor(
         private set
     var recyclableList = mutableStateListOf<SearchableListModel>()
         private set
+    var result = mutableStateOf(
+        ResultResponseModel(
+            id = 0L,
+            title = "", subTitle = "",
+            recycleMethod = "",
+            recycleTip = "",
+            recycleCaution = "",
+            imageUrl = "",
+            recyclablesType = "",
+            recycleMark = ""
+        )
+    )
+        private set
+
 
     fun search(search: String) = viewModelScope.launch {
         searchUseCase(search = search)
@@ -66,15 +86,32 @@ class RecyclablesViewModel @Inject constructor(
             }
     }
 
-    fun saveSearch(result: SearchResponseModel) {
-        title.value = result.title
-        imageUrl.value = result.imageUrl
-        recycleMethod.value = result.recycleMethod
-        recyclablesType.value = result.recyclablesType
+    fun result(recyclablesType: String) = viewModelScope.launch {
+        resultUseCase(recyclablesType = recyclablesType)
+            .onSuccess {
+                it.catch { remoteError ->
+                    _resultResponse.value = remoteError.errorHandling()
+                }.collect { response ->
+                    _resultResponse.value = Event.Success(data = response)
+                }
+            }.onFailure {
+                _resultResponse.value = it.errorHandling()
+            }
     }
 
-    fun saveSearchableList(result: List<SearchableListModel>) {
+    fun saveSearch(data: SearchResponseModel) {
+        title.value = data.title
+        imageUrl.value = data.imageUrl
+        recycleMethod.value = data.recycleMethod
+        recyclablesType.value = data.recyclablesType
+    }
+
+    fun saveSearchableList(data: List<SearchableListModel>) {
         recyclableList.clear()
-        recyclableList.addAll(result)
+        recyclableList.addAll(data)
+    }
+
+    fun saveResult(data: ResultResponseModel) {
+        result.value = data
     }
 }
