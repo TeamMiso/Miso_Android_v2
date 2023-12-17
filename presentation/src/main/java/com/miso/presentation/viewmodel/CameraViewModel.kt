27557dd812daca33888e220c9ac1da25defba820
GSM-Minseok <ms0818000@gmail.com>
2023-12-17 22:44:53 +0900

    :memo: :: Add CameraViewModel.kt

---
+++ presentation/src/main/java/com/miso/presentation/viewmodel/CameraViewModel.kt
@@ -0,0 +1,24 @@
+package com.miso.presentation.viewmodel
+
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.miso.presentation.ui.camera.state.CapturedState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CameraViewModel @Inject constructor() : ViewModel() {
+    private val _capturedImgBitmapState = MutableStateFlow(CapturedState())
+    val captureImgBitmapState = _capturedImgBitmapState.asStateFlow()
+
+    fun loadImgBitmap(bitmap: Bitmap){
+        viewModelScope.launch {
+            _capturedImgBitmapState.value.capturedImage?.recycle()
+            _capturedImgBitmapState.value = _capturedImgBitmapState.value.copy(capturedImage = bitmap)
+        }
+    }
+}
