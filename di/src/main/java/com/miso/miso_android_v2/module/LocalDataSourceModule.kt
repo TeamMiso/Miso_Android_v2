61030983aa01719af032942a6f59020966d37c80
송현서 <s22026@gsm.hs.kr>
2023-12-17 22:44:17 +0900

    :sparkles: :: Add search history to module

--- di/src/main/java/com/miso/miso_android_v2/module/LocalDataSourceModule.kt
+++ di/src/main/java/com/miso/miso_android_v2/module/LocalDataSourceModule.kt
@@ -2,6 +2,8 @@ package com.miso.miso_android_v2.module
 
 import com.miso.data.local.datasource.auth.LocalAuthDataSource
 import com.miso.data.local.datasource.auth.LocalAuthDataSourceImpl
+import com.miso.data.local.datasource.recyclables.LocalRecyclablesDataSource
+import com.miso.data.local.datasource.recyclables.LocalRecyclablesDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,4 +16,9 @@ abstract class LocalDataSourceModule {
     abstract fun provideLocalAuthDataSource(
         localAuthDataSourceImpl: LocalAuthDataSourceImpl
     ): LocalAuthDataSource
+
+    @Binds
+    abstract fun provideLocalRecyclablesDataSource(
+        localRecyclablesDataSourceImpl: LocalRecyclablesDataSourceImpl
+    ): LocalRecyclablesDataSource
 }
