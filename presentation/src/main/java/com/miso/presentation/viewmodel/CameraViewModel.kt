bcdc7127bb206d946421f80dd4a611199ab49bf9
GSM-Minseok <ms0818000@gmail.com>
2023-12-18 11:19:23 +0900

    :fire: :: Fire viewModel not use import

--- presentation/src/main/java/com/miso/presentation/viewmodel/CameraViewModel.kt
+++ presentation/src/main/java/com/miso/presentation/viewmodel/CameraViewModel.kt
@@ -1,7 +1,6 @@
 package com.miso.presentation.viewmodel
 
 import android.graphics.Bitmap
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.miso.viewmodel.util.Event
@@ -13,12 +12,10 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.internal.http.RealResponseBody
 import java.io.ByteArrayOutputStream
 import javax.inject.Inject
 
