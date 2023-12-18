37829bbb32e1858341e3154355b7798443f5400e
송현서 <s22026@gsm.hs.kr>
2023-12-19 00:03:28 +0900

    :sparkles: :: Write the logic purchase

--- data/src/main/java/com/miso/data/remote/api/UserAPI.kt
+++ data/src/main/java/com/miso/data/remote/api/UserAPI.kt
@@ -3,12 +3,11 @@ package com.miso.data.remote.api
 import com.miso.data.remote.dto.user.response.PointResponse
 import com.miso.data.remote.dto.user.response.UserInfoResponse
 import retrofit2.http.GET
-import retrofit2.http.POST
 
 interface UserAPI {
     @GET("user")
     suspend fun getUserInfo(): UserInfoResponse
 
-    @POST("user/point")
+    @GET("user/point")
     suspend fun getPoint(): PointResponse
 }
