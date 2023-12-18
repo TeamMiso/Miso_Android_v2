d3531902d020717fcbfe1e25886e22454a77b5bc
GSM-Minseok <ms0818000@gmail.com>
2023-12-18 10:25:44 +0900

    :memo: :: Add ai remote data source

--- data/src/main/java/com/miso/data/remote/datasource/recyclables/RecyclablesDataSource.kt
+++ data/src/main/java/com/miso/data/remote/datasource/recyclables/RecyclablesDataSource.kt
@@ -1,9 +1,12 @@
 package com.miso.data.remote.datasource.recyclables
 
+import com.miso.data.remote.dto.recyclables.response.AiListResponse
 import com.miso.data.remote.dto.recyclables.response.ResultResponse
 import com.miso.data.remote.dto.recyclables.response.SearchResponse
 import com.miso.data.remote.dto.recyclables.response.SearchableListResponse
 import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
+import retrofit2.http.Multipart
 
 interface RecyclablesDataSource {
     suspend fun search(search: String): Flow<SearchResponse>
@@ -11,4 +14,6 @@ interface RecyclablesDataSource {
     suspend fun searchableList(): Flow<SearchableListResponse>
 
     suspend fun result(recyclablesType: String): Flow<ResultResponse>
+
+    suspend fun getAiAnswerList(recyclables: MultipartBody.Part): Flow<AiListResponse>
 }
--- data/src/main/java/com/miso/data/remote/datasource/recyclables/RecyclablesDataSourceImpl.kt
+++ data/src/main/java/com/miso/data/remote/datasource/recyclables/RecyclablesDataSourceImpl.kt
@@ -1,6 +1,8 @@
 package com.miso.data.remote.datasource.recyclables
 
+import android.util.Log
 import com.miso.data.remote.api.RecyclablesAPI
+import com.miso.data.remote.dto.recyclables.response.AiListResponse
 import com.miso.data.remote.dto.recyclables.response.ResultResponse
 import com.miso.data.remote.dto.recyclables.response.SearchResponse
 import com.miso.data.remote.dto.recyclables.response.SearchableListResponse
@@ -9,6 +11,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import okhttp3.MultipartBody
+import retrofit2.http.Multipart
 import javax.inject.Inject
 
 class RecyclablesDataSourceImpl @Inject constructor(
@@ -34,6 +38,15 @@ class RecyclablesDataSourceImpl @Inject constructor(
         emit(
             MisoApiHandler<ResultResponse>()
                 .httpRequest { api.result(recyclablesType = recyclablesType) }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun getAiAnswerList(recyclables: MultipartBody.Part): Flow<AiListResponse> = flow {
+        Log.d("testt",recyclables.toString())
+        emit(
+            MisoApiHandler<AiListResponse>()
+                .httpRequest { api.getAiAnswerList(recyclables = recyclables) }
                 .sendRequest()
         )
     }.flowOn(Dispatchers.IO)
