cf8aa68525ee265da9f36dc98317d7430c052ab2
송현서 <s22026@gsm.hs.kr>
2023-12-18 09:14:49 +0900

    :sparkles: :: Add shop to ShopDatasource

---
+++ data/src/main/java/com/miso/data/remote/datasource/shop/ShopDatasourceImpl.kt
@@ -0,0 +1,22 @@
+package com.miso.data.remote.datasource.shop
+
+import com.miso.data.remote.api.ShopAPI
+import com.miso.data.remote.dto.shop.response.ShopListResponse
+import com.miso.data.util.MisoApiHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class ShopDatasourceImpl @Inject constructor(
+    private val api: ShopAPI
+): ShopDatasource {
+    override suspend fun getShopList(): Flow<ShopListResponse> = flow {
+        emit(
+            MisoApiHandler<ShopListResponse>()
+                .httpRequest { api.shopList() }
+                .sendRequest()
+        )
+    }.flowOn(Dispatchers.IO)
+}
