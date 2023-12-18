300bda86593b317107ff56b44163406ee34db2ec
송현서 <s22026@gsm.hs.kr>
2023-12-18 14:06:40 +0900

    :sparkles: :: Add shop detail to ShopRepository

--- data/src/main/java/com/miso/data/repository/ShopRepositoryImpl.kt
+++ data/src/main/java/com/miso/data/repository/ShopRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.miso.data.repository
 
 import com.miso.data.remote.datasource.shop.ShopDataSource
 import com.miso.data.remote.dto.shop.response.toShopModel
+import com.miso.domain.model.shop.response.ShopListDetailResponseModel
 import com.miso.domain.model.shop.response.ShopListResponseModel
 import com.miso.domain.repository.ShopRepository
 import kotlinx.coroutines.flow.Flow
@@ -13,5 +14,9 @@ class ShopRepositoryImpl @Inject constructor(
 ): ShopRepository {
     override suspend fun shopList(): Flow<ShopListResponseModel> {
         return remoteShopDatasource.shopList().map { it.toShopModel() }
+    }
+
+    override suspend fun shopListDetail(id: Long): Flow<ShopListDetailResponseModel> {
+        return remoteShopDatasource.shopListDetail(id = id).map { it.toShopModel() }
     }
 }
--- domain/src/main/java/com/miso/domain/repository/ShopRepository.kt
+++ domain/src/main/java/com/miso/domain/repository/ShopRepository.kt
@@ -1,8 +1,11 @@
 package com.miso.domain.repository
 
+import com.miso.domain.model.shop.response.ShopListDetailResponseModel
 import com.miso.domain.model.shop.response.ShopListResponseModel
 import kotlinx.coroutines.flow.Flow
 
 interface ShopRepository {
     suspend fun shopList(): Flow<ShopListResponseModel>
+
+    suspend fun shopListDetail(id: Long): Flow<ShopListDetailResponseModel>
 }
