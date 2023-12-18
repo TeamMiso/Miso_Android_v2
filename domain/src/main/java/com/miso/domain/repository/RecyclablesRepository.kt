90185a4b0953f712dc498b0b6fa45d263e92aa48
GSM-Minseok <ms0818000@gmail.com>
2023-12-18 10:26:00 +0900

    :memo: :: Add ai remote repository

--- domain/src/main/java/com/miso/domain/repository/RecyclablesRepository.kt
+++ domain/src/main/java/com/miso/domain/repository/RecyclablesRepository.kt
@@ -1,9 +1,11 @@
 package com.miso.domain.repository
 
+import com.miso.domain.model.recyclables.response.AiListResponseModel
 import com.miso.domain.model.recyclables.response.ResultResponseModel
 import com.miso.domain.model.recyclables.response.SearchResponseModel
 import com.miso.domain.model.recyclables.response.SearchableListResponseModel
 import kotlinx.coroutines.flow.Flow
+import okhttp3.MultipartBody
 
 interface RecyclablesRepository {
     suspend fun search(search: String): Flow<SearchResponseModel>
@@ -15,4 +17,6 @@ interface RecyclablesRepository {
     suspend fun saveSearchHistory(searchHistory: SearchResponseModel)
 
     suspend fun getSearchHistory(): Flow<List<SearchResponseModel>>
+
+    suspend fun getAiAnswerList(recyclables: MultipartBody.Part): Flow<AiListResponseModel>
 }
