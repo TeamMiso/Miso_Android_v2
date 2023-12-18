c18c74d1591c47c93216e7dbfe8e13eaa90c47a6
GSM-Minseok <ms0818000@gmail.com>
2023-12-18 10:26:36 +0900

    :memo: :: Modify getAiAnswerList

--- data/src/main/java/com/miso/data/remote/api/RecyclablesAPI.kt
+++ data/src/main/java/com/miso/data/remote/api/RecyclablesAPI.kt
@@ -1,13 +1,16 @@
 package com.miso.data.remote.api
 
-import com.miso.data.remote.dto.recyclables.request.AiRequest
 import com.miso.data.remote.dto.recyclables.response.AiListResponse
 import com.miso.data.remote.dto.recyclables.response.ResultResponse
 import com.miso.data.remote.dto.recyclables.response.SearchResponse
 import com.miso.data.remote.dto.recyclables.response.SearchableListResponse
+import okhttp3.MultipartBody
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RecyclablesAPI {
@@ -24,8 +27,9 @@ interface RecyclablesAPI {
         @Query("recyclablesType") recyclablesType: String
     ): ResultResponse
 
+    @Multipart
     @POST("recyclables/process")
     suspend fun getAiAnswerList(
-        @Body body: AiRequest
+        @Part recyclables: MultipartBody.Part
     ): AiListResponse
 }
