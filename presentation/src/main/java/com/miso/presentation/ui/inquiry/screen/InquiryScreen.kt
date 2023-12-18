14ae4ebecacfe58df89b26397d1d84cfb61f888a
GSM-Minseok <ms0818000@gmail.com>
2023-12-18 23:42:23 +0900

    :sparkles: :: Show bottom sheet

--- presentation/src/main/java/com/miso/presentation/ui/inquiry/screen/InquiryScreen.kt
+++ presentation/src/main/java/com/miso/presentation/ui/inquiry/screen/InquiryScreen.kt
@@ -4,27 +4,63 @@ import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.miso.design_system.theme.MisoTheme
 import com.miso.presentation.ui.inquiry.component.InquiryImageButton
 import com.miso.presentation.ui.inquiry.component.InquiryTextTextField
 import com.miso.presentation.ui.inquiry.component.InquiryTitleTextField
 import com.miso.presentation.ui.inquiry.component.InquiryTopBar
+import com.miso.presentation.ui.inquiry.component.bottomsheet.SelectPhotoPathBottomSheet
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun InquiryScreen() {
-    MisoTheme { colors, typography ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(colors.WHITE)
-        ) {
-            InquiryTopBar(onInquiryClick = {}, onBackClick = {})
-            InquiryTitleTextField()
-            InquiryImageButton(onClick = {}, selectedImageUri = Uri.EMPTY, capturedImage = null)
-            InquiryTextTextField()
+    var bottomSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
+    val bottomSheetScope = rememberCoroutineScope()
+
+    ModalBottomSheetLayout(
+        sheetContent = {
+            SelectPhotoPathBottomSheet(
+                bottomSheetState = bottomSheetState,
+                selectedImageUri = {},
+                onCameraClick = {}
+            )
+        },
+        sheetShape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
+        sheetState = bottomSheetState
+    ) {
+        MisoTheme { colors, typography ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(colors.WHITE)
+                    .statusBarsPadding()
+            ) {
+                InquiryTopBar(onInquiryClick = {}, onBackClick = {})
+                InquiryTitleTextField()
+                InquiryImageButton(
+                    onClick = {
+                        bottomSheetScope.launch {
+                            bottomSheetState.show()
+                        }
+                    },
+                    selectedImageUri = Uri.EMPTY,
+                    capturedImage = null
+                )
+                InquiryTextTextField()
+            }
         }
     }
 }
