f8bc4e750f5c91ea10383c72f02363a93a677597
GSM-Minseok <ms0818000@gmail.com>
2023-12-18 10:27:00 +0900

    :memo: :: Add AiListResponse.kt

--- data/src/main/java/com/miso/data/remote/dto/recyclables/response/AiListResponse.kt
+++ data/src/main/java/com/miso/data/remote/dto/recyclables/response/AiListResponse.kt
@@ -1,5 +1,8 @@
 package com.miso.data.remote.dto.recyclables.response
 
+import com.miso.domain.model.recyclables.response.AiListModel
+import com.miso.domain.model.recyclables.response.AiListResponseModel
+
 data class AiListResponse(
     val recyclablesList: List<AiList>
 )
@@ -9,11 +12,27 @@ data class AiList(
     val title: String,
     val subTitle: String,
     val recycleMethod: String,
-    val recyleTip: String,
-    val recylcleCaution: String,
+    val recycleTip: String,
+    val recycleCaution: String,
     val imageUrl: String,
     val recyclablesType: String,
     val recycleMark: String
 )
 
+fun AiListResponse.toAiListResponseModel() =
+    AiListResponseModel(
+        recyclablesList = this.recyclablesList.map { it.toAiListModel() }
+    )
 
+fun AiList.toAiListModel() =
+    AiListModel(
+        id = this.id,
+        title = this.title,
+        subTitle = this.subTitle,
+        recycleMethod = this.recycleMethod,
+        recycleTip = this.recycleTip,
+        recycleCaution = this.recycleCaution,
+        imageUrl = this.imageUrl,
+        recyclablesType = this.recyclablesType,
+        recycleMark = this.recycleMark
+    )
