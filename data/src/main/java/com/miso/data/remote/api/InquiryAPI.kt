e2558b0758a0c0921be4dcfc839e4340e65a32ae
송현서 <s22026@gsm.hs.kr>
2023-12-19 16:46:55 +0900

    :sparkles: :: Add inquiry list InquiryAPI

--- data/src/main/java/com/miso/data/remote/api/InquiryAPI.kt
+++ data/src/main/java/com/miso/data/remote/api/InquiryAPI.kt
@@ -1,7 +1,9 @@
 package com.miso.data.remote.api
 
+import com.miso.data.remote.dto.inquiry.response.InquiryListResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
@@ -13,4 +15,10 @@ interface InquiryAPI {
         @Part filePart: MultipartBody.Part?,
         @Part("inquiry") inquiryPart: RequestBody
     )
+
+    @GET("inquiry")
+    suspend fun getInquiryList(): InquiryListResponse
+
+    @GET("inquiry/all")
+    suspend fun getInquiryListAll(): InquiryListResponse
 }
