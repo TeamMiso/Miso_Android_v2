08985a68b1e6d36aafee032daf8c37636b524c6d
송현서 <s22026@gsm.hs.kr>
2023-12-19 20:20:32 +0900

    :sparkles: :: Add purchase list to PurchaseAPI

--- data/src/main/java/com/miso/data/remote/api/PurchaseAPI.kt
+++ data/src/main/java/com/miso/data/remote/api/PurchaseAPI.kt
@@ -1,5 +1,7 @@
 package com.miso.data.remote.api
 
+import com.miso.data.remote.dto.purchase.response.PurchaseListResponse
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -8,4 +10,7 @@ interface PurchaseAPI {
     suspend fun purchase(
         @Path("id") id: Long
     )
+
+    @GET("purchase")
+    suspend fun getPurchaseList(): PurchaseListResponse
 }
