160ed26484ce5ad1beeda86c87f88fd05fb4a6c8
송현서 <s22026@gsm.hs.kr>
2023-12-19 20:20:40 +0900

    :sparkles: :: Add purchase list to PurchaseDataSource

--- data/src/main/java/com/miso/data/remote/datasource/purchase/PurchaseDataSource.kt
+++ data/src/main/java/com/miso/data/remote/datasource/purchase/PurchaseDataSource.kt
@@ -1,7 +1,10 @@
 package com.miso.data.remote.datasource.purchase
 
+import com.miso.data.remote.dto.purchase.response.PurchaseListResponse
 import kotlinx.coroutines.flow.Flow
 
 interface PurchaseDataSource {
     suspend fun purchase(id: Long): Flow<Unit>
+
+    suspend fun getPurchaseList(): Flow<PurchaseListResponse>
 }
