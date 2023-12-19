639d88cb13c83c9694693076f1f28a49733974ce
GSM-Minseok <ms0818000@gmail.com>
2023-12-19 18:03:01 +0900

    :lipstick: :: Add InquiryDateText.kt

---
+++ presentation/src/main/java/com/miso/presentation/ui/inquiry/component/InquiryDateText.kt
@@ -0,0 +1,20 @@
+package com.miso.presentation.ui.inquiry.component
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import com.miso.design_system.theme.MisoTheme
+
+@Composable
+fun InquiryDateText(
+    date: String
+) {
+    MisoTheme { colors, typography ->
+        Text(
+            text = date,
+            style = typography.buttonSmall,
+            fontWeight = FontWeight.Normal,
+            color = colors.GREYSCALE2
+        )
+    }
+}
