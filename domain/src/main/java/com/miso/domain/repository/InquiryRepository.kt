890a6496602266c9c5ee749d2b9c2f00d7f6ddc4
송현서 <s22026@gsm.hs.kr>
2023-12-19 16:47:30 +0900

    :sparkles: :: Add inquiry list InquiryRepository

--- domain/src/main/java/com/miso/domain/repository/InquiryRepository.kt
+++ domain/src/main/java/com/miso/domain/repository/InquiryRepository.kt
@@ -1,9 +1,14 @@
 package com.miso.domain.repository
 
+import com.miso.domain.model.inquiry.response.InquiryListResponseModel
 import kotlinx.coroutines.flow.Flow
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 
 interface InquiryRepository {
     suspend fun requestInquiry(filePart: MultipartBody.Part?, inquiryPart: RequestBody): Flow<Unit>
+
+    suspend fun getInquiryList(): Flow<InquiryListResponseModel>
+
+    suspend fun getInquiryListAll(): Flow<InquiryListResponseModel>
 }
