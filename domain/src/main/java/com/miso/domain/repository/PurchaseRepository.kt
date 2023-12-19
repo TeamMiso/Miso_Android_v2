986f5337b495732372c299dad952a77d5605f851
송현서 <s22026@gsm.hs.kr>
2023-12-19 20:20:48 +0900

    :sparkles: :: Add purchase list to PurchaseRepository

--- domain/src/main/java/com/miso/domain/repository/PurchaseRepository.kt
+++ domain/src/main/java/com/miso/domain/repository/PurchaseRepository.kt
@@ -1,7 +1,10 @@
 package com.miso.domain.repository
 
+import com.miso.domain.model.purchase.response.PurchaseListResponseModel
 import kotlinx.coroutines.flow.Flow
 
 interface PurchaseRepository {
     suspend fun purchase(id: Long): Flow<Unit>
+
+    suspend fun getPurchaseList(): Flow<PurchaseListResponseModel>
 }
