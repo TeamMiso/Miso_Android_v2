d1ad2b5d839c973785adc55354aa2e7c05f9dc87
송현서 <s22026@gsm.hs.kr>
2023-12-21 18:10:02 +0900

    :sparkles: :: Add send answer to InquiryAPI

--- data/src/main/java/com/miso/data/remote/api/InquiryAPI.kt
+++ data/src/main/java/com/miso/data/remote/api/InquiryAPI.kt
@@ -1,11 +1,14 @@
 package com.miso.data.remote.api
 
+import com.miso.data.remote.dto.inquiry.request.AnswerRequest
 import com.miso.data.remote.dto.inquiry.response.InquiryListDetailResponse
 import com.miso.data.remote.dto.inquiry.response.InquiryListResponse
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Multipart
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Path
@@ -28,4 +31,10 @@ interface InquiryAPI {
     suspend fun getInquiryListDetail(
         @Path("id") id: Long
     ): InquiryListDetailResponse
+
+    @PATCH("inquiry/respond/{id}")
+    suspend fun sendAnswer(
+        @Path("id") id: Long,
+        @Body body: AnswerRequest
+    )
 }
