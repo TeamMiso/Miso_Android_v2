abddcf6c3c91985e51f9b5303c8429bc4a23704f
송현서 <s22026@gsm.hs.kr>
2023-12-21 15:31:38 +0900

    :lipstick: :: Modify inquiry answer screen

--- presentation/src/main/java/com/miso/presentation/ui/inquiry/screen/InquiryAnswerScreen.kt
+++ presentation/src/main/java/com/miso/presentation/ui/inquiry/screen/InquiryAnswerScreen.kt
@@ -15,17 +15,22 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.miso.design_system.theme.MisoTheme
 import com.miso.presentation.ui.inquiry.component.InquiryContentTextField
 import com.miso.presentation.ui.inquiry.component.InquiryDateText
 import com.miso.presentation.ui.inquiry.component.InquiryTitleText
 import com.miso.presentation.ui.inquiry.component.InquiryTopBar
+import com.miso.presentation.ui.util.toDateString
+import com.miso.presentation.viewmodel.InquiryViewModel
 
 @Composable
-fun InquiryAnswerScreen() {
+fun InquiryAnswerScreen(
+    viewModel: InquiryViewModel,
+    onBackClick: () -> Unit
+) {
     var content by remember { mutableStateOf("") }
+
     MisoTheme { colors, typography ->
         Column(
             modifier = Modifier
@@ -33,15 +38,23 @@ fun InquiryAnswerScreen() {
                 .background(colors.WHITE)
                 .statusBarsPadding()
         ) {
-            InquiryTopBar(onInquiryClick = {}, onBackClick = {},isUser = false)
+            InquiryTopBar(
+                onInquiryClick = {
+
+                },
+                onBackClick = {
+                    onBackClick()
+                },
+                isUser = false
+            )
             Spacer(modifier = Modifier.height(8.dp))
-            InquiryTitleText(title = "실패하면 반역, 성공하면 혁명 아입니까?!")
+            InquiryTitleText(title = viewModel.inquiryListDetail.value.title)
             Spacer(modifier = Modifier.height(8.dp))
             Row(modifier = Modifier
                 .fillMaxWidth()
             ) {
                 Spacer(modifier = Modifier.width(16.dp))
-                InquiryDateText(date = "1979.12.12")
+                InquiryDateText(date = viewModel.inquiryListDetail.value.inquiryDate.toDateString())
             }
             InquiryContentTextField(
                 content = content,
@@ -51,10 +64,4 @@ fun InquiryAnswerScreen() {
             )
         }
     }
-}
-
-@Composable
-@Preview(showBackground = true)
-fun InquiryAnswerScreenPreview() {
-    InquiryAnswerScreen()
 }
